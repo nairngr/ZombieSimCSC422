2dfe43090789276c79f91a9e0c65837064bedff4
jhpetrie <150563346+jhpetrie@users.noreply.github.com>
2023-11-20 15:46:28 -0600

    Cleanup Constructors and added quick test to Main
    Tank constructor was fixed of remnants from a previous version. Added some quick code to Main to demonstrate how this might work.

--- zombiegame/src/main/java/com/csc422/Main.java
+++ zombiegame/src/main/java/com/csc422/Main.java
@@ -3,6 +3,15 @@ package com.csc422;
 // Main to be updated later
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Tank tank1 = new Tank();
+        Soldier sgtJohn = new Soldier();
+
+        System.out.println("There is a tank with " + tank1.getAttack() + " attack power and " + tank1.getHealth() + " health");
+
+        tank1.setHealth(tank1.getHealth() - sgtJohn.getAttack());
+
+        System.out.println("The tank is attacked by sgtJohn and now has " + tank1.getHealth() + " health");
+        
+
     }
 }
--- zombiegame/src/main/java/com/csc422/Tank.java
+++ zombiegame/src/main/java/com/csc422/Tank.java
@@ -2,7 +2,7 @@ package com.csc422;
 
 // Tank Class
 public class Tank extends Character {
-    public Tank(int health, int attack) {
+    public Tank() {
         this.health = 150;
         this.attack = 20;
         this.survivor = false;
